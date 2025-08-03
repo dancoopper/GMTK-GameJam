4bb74f9bfeb9471573e0e69d66c3d7f9822ecd71
Jonker88 <jonathan.h.cao88@gmail.com>
2025-08-02 20:59:30 -0400

    added destination points

--- scenes/portal.gd
+++ scenes/portal.gd
@@ -1,5 +1,12 @@
 extends Area2D
 
+@export var destination_point_path: NodePath
+var destination_point: Node2D
+
+func _ready():
+	if destination_point_path:
+		destination_point = get_node(destination_point_path)
+
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		body.set_position($DestinationPoint.global_position)
+	if body.is_in_group("Player") and destination_point:
+		body.set_position(destination_point.global_position)
